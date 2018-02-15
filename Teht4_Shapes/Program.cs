7f5c4ef3160e790e1d8e516f0c71b9cbb1dfdc8f
Antti Virkkunen <antti.virkkunen@gmail.com>
2018-02-15 18:50:28 +0200

    Teht4 - shapes

---
+++ Teht4_Shapes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht4_Shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Shape> shapeList = new List<Shape>();
+
+            shapeList.Add(new Circle(1));
+            shapeList.Add(new Circle(2));
+            shapeList.Add(new Circle(3));
+            shapeList.Add(new Rectangle(10, 20));
+            shapeList.Add(new Rectangle(20, 30));
+            shapeList.Add(new Rectangle(40, 50));
+
+            foreach (Shape s in shapeList)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ Teht4_Shapes/Shape.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht4_Shapes
+{
+    public abstract class Shape
+    {
+        public string Name { get; set; }
+
+        public abstract double Area();
+        public abstract double Circumference();
+    }
+
+    public class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            this.Name = "Rectangle";
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public override double Area()
+        {
+            return Width * Height;
+        }
+
+        public override double Circumference()
+        {
+            return (Width + Height) * 2;
+        }
+
+        public override string ToString()
+        {
+            return Name + " Width: " + Width + " Height: " + Height + " Area: " + Area() + " Circumference: " + Circumference();
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+        
+        public Circle(double radius)
+        {
+            this.Name = "Circle";
+            this.Radius = radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public override double Circumference()
+        {
+            return Math.PI * Radius * 2;
+        }
+
+        public override string ToString()
+        {
+            return Name + " Radius: " + Radius + " Area: " + Area() + " Circumference: " + Circumference();
+        }
+    }
+}
