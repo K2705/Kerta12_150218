4cd9d34b65a7b52a5cca8aa48ac997bad6a1b3d4
Antti Virkkunen <antti.virkkunen@gmail.com>
2018-02-15 18:00:00 +0200

    Teht2 - osokset

---
+++ Teht2_Ostokset/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht2_Ostokset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> prods = new List<Product>();
+
+            prods.Add(new Product("Milk", 1.4));
+            prods.Add(new Product("Beer", 2.2));
+            prods.Add(new Product("Butter", 3.4));
+            prods.Add(new Product("Cheese", 4.2));
+
+            Console.WriteLine("Products in list:");
+            foreach (Product p in prods)
+            {
+                Console.WriteLine(p.Name + " " + p.Price + "e");
+            }
+        }
+    }
+}
